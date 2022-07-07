992ee4863210742d19abf9e98e30a19ade405ea8
Phillipi Alves <phixelz@gmail.com>
2022-07-07 19:06:38 -0300

    theme created

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import theme from "./src/global/styles/theme";
+
 import { SignIn } from "./src/pages/SignIn";
 import { Home } from "./src/pages/Home";
 import { Perfil } from "./src/pages/Perfil";
@@ -14,45 +19,47 @@ const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{ headerShown: false }}
-        initialRouteName="SignIn"
-      >
-        <Stack.Screen
-          name="SignIn"
-          component={SignIn}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-        />
-        <Stack.Screen
-          name="Perfil"
-          component={Perfil}
-        />
-        <Stack.Screen
-          name="Search"
-          component={Search}
-        />
-        <Stack.Screen
-          name="LearnProgram"
-          component={LearnProgram}
-        />
-        <Stack.Screen
-          name="Suggestions"
-          component={Suggestions}
-        />
-        <Stack.Screen
-          name="Course"
-          component={Course}
-        />
-        <Stack.Screen
-          name="Cart"
-          component={Cart}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="SignIn"
+        >
+          <Stack.Screen
+            name="SignIn"
+            component={SignIn}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+          />
+          <Stack.Screen
+            name="Perfil"
+            component={Perfil}
+          />
+          <Stack.Screen
+            name="Search"
+            component={Search}
+          />
+          <Stack.Screen
+            name="LearnProgram"
+            component={LearnProgram}
+          />
+          <Stack.Screen
+            name="Suggestions"
+            component={Suggestions}
+          />
+          <Stack.Screen
+            name="Course"
+            component={Course}
+          />
+          <Stack.Screen
+            name="Cart"
+            component={Cart}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
